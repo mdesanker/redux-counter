c7e7946a817efd56574b35a93cf4e81e6b1f9384
Michael Desanker <mdesanker@gmail.com>
2021-12-17 09:13:10 -0500

    Create basic ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import styled from "styled-components";
+
 const App = () => {
-  return <div>Hello world!</div>;
+  return (
+    <Wrapper>
+      <h1>Counter</h1>
+      <div>
+        <button>+ 1</button>
+        <button>- 1</button>
+      </div>
+      <form action="">
+        <label htmlFor="amount">Enter amount:</label>
+        <input type="text" name="amount" id="amount" />
+        <button>+ Amount</button>
+      </form>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  padding: 3rem;
+`;
 
 export default App;
